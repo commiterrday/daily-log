118347f3d3fe1b078dde922191549dc97fbdb252
commiterrday <commiterrday@gmail.com>
2015-02-10 23:59:28 +0100

    Add all.json view

--- daily_log/weight/urls.py
+++ daily_log/weight/urls.py
@@ -4,4 +4,5 @@ from weight import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^all.json$', views.alljson, name='alljson'),
 )
--- daily_log/weight/views.py
+++ daily_log/weight/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 from weight.models import Weight
 
-# Create your views here.
+
 def index(request):
     points = Weight.objects.all()
     context = {'points': points }
     return render(request, 'weight/index.html', context)
+
+
+def alljson(request):
+    # Convert to List, since ValueListQuerySet is not JSON-serializable
+    # Ref: http://blog.mourafiq.com/post/47739842739/is-not-json-serializable-django
+    points = list(Weight.objects.values('time', 'kg', 'body_fat'))
+
+    #for p in points:
+    #    p['time'] = str(p['time']) # TODO: avoid losing the TZ
+
+    return JsonResponse({'points': points })
