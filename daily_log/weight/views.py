ae5bad3dfcb3841f2ffcf8bf5c5473a8b979af33
commiterrday <commiterrday@gmail.com>
2015-02-11 00:05:12 +0100

    Note on JSON/Array hack

--- daily_log/weight/views.py
+++ daily_log/weight/views.py
@@ -18,4 +18,8 @@ def alljson(request):
     #for p in points:
     #    p['time'] = str(p['time']) # TODO: avoid losing the TZ
 
+    # Security concerns: is JSON/Array [1] an issue with the nested array? Note this isn't
+    # an issue with EcmaScript 5, according to the Django docs [2].
+    # [1] http://incompleteness.me/blog/2007/03/05/json-is-not-as-safe-as-people-think-it-is/
+    # [2] https://docs.djangoproject.com/en/1.7/ref/request-response/#serializing-non-dictionary-objects
     return JsonResponse({'points': points })
