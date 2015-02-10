bddf9162eda28c29da78281cb5d98b790efcee41
commiterrday <commiterrday@gmail.com>
2015-02-10 23:20:24 +0100

    List all points in log/ index view

--- daily_log/weight/views.py
+++ daily_log/weight/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from weight.models import Weight
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the log index.")
+    allpts = Weight.objects.all()
+    output = '<ul>\n'
+    output += '\n'.join(
+            ['<li>' + str(w.time) + ': %.1f kg %.1f%% BF</li>' % (w.kg, w.body_fat) for w in allpts]
+            )
+    output += '</ul>\n'
+    return HttpResponse(output)
