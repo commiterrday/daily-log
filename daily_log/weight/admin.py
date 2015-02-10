d1ba2bbbeb6ca9e9fe5e0d9b0e578b4922e2707d
commiterrday <commiterrday@gmail.com>
2015-02-11 00:36:41 +0100

    Improve admin page for Weight model

--- daily_log/weight/admin.py
+++ daily_log/weight/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from weight.models import Weight
 
+class WeightAdmin(admin.ModelAdmin):
+    list_display = ('time', 'kg', 'body_fat')
+
 # Register your models here.
-admin.site.register(Weight)
+admin.site.register(Weight, WeightAdmin)
