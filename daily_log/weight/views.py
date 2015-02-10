0719ac5c6cec9709bd69e65034fd0cacb3974172
commiterrday <commiterrday@gmail.com>
2015-02-10 23:37:06 +0100

    Switch to template + table

--- daily_log/weight/views.py
+++ daily_log/weight/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from weight.models import Weight
 
 # Create your views here.
 def index(request):
-    allpts = Weight.objects.all()
-    output = '<ul>\n'
-    output += '\n'.join(
-            ['<li>' + str(w.time) + ': %.1f kg %.1f%% BF</li>' % (w.kg, w.body_fat) for w in allpts]
-            )
-    output += '</ul>\n'
-    return HttpResponse(output)
+    points = Weight.objects.all()
+    context = {'points': points }
+    return render(request, 'weight/index.html', context)
